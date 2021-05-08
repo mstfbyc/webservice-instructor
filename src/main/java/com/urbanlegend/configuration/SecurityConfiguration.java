68f4eca7121124771810fded2ae5180a1cbad48b
Mustafa Boyaci <mustafaboyaci@hotmail.com>
2021-05-09 02:15:11 +0300

    Bolum 5 Bitmiş hali Hata Cevabı öncesi son kod

--- src/main/java/com/urbanlegend/configuration/SecurityConfiguration.java
+++ src/main/java/com/urbanlegend/configuration/SecurityConfiguration.java
@@ -3,23 +3,14 @@ package com.urbanlegend.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -30,12 +21,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //Request içinde html oluşturduğu token devre dışı bırakır.
         http.csrf().disable();
-        //BasicAuthenticationEntryPoint
+        //BasicAuthenticationEntryPoint sayfanın tepesinde kullanıcı adı parola istememesi için
         http.httpBasic().authenticationEntryPoint(new AuthEntryPoint());
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/1.0/auth").authenticated()
                 .and()
                 .authorizeRequests().anyRequest().permitAll();
+        //Tüm requestlerde header da auth bilgisinin olmasını sağlar
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
