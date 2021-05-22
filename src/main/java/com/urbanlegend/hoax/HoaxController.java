54e7893c4fc2a53d233c126e1a21959ebd4e7227
Mustafa Boyaci <mustafaboyaci@hotmail.com>
2021-05-22 15:31:34 +0300

    Bölüm 17 Hoax Spefication and Pagination

--- src/main/java/com/urbanlegend/hoax/HoaxController.java
+++ src/main/java/com/urbanlegend/hoax/HoaxController.java
@@ -12,6 +12,11 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController("/api/1.0")
 public class HoaxController {
 
@@ -32,5 +37,26 @@ public class HoaxController {
     @GetMapping("/users/{username}/hoaxes")
     ResponseEntity<Page<HoaxVM>> getUserHoaxes(@PathVariable String username, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page){
         return ResponseEntity.ok(hoaxService.getHoaxesOfUser(username, page).map(HoaxVM::new));
+    }
+
+    @GetMapping({"/hoaxes/{id:[0-9]+}", "/users/{username}/hoaxes/{id:[0-9]+}"})
+    ResponseEntity<?> getHoaxesRelative(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page,
+                                        @PathVariable Long id,
+                                        @PathVariable(required=false) String username,
+                                        @RequestParam(name="count", required = false, defaultValue = "false") boolean count,
+                                        @RequestParam(name="direction", defaultValue = "before") String direction){
+        if(count) {
+            long newHoaxCount = hoaxService.getNewHoaxesCount(id, username);
+            Map<String, Long> response = new HashMap<>();
+            response.put("count", newHoaxCount);
+            return ResponseEntity.ok(response);
+        }
+        if(direction.equals("after")) {
+            List<HoaxVM> newHoaxes = hoaxService.getNewHoaxes(id, username, page.getSort())
+                    .stream().map(HoaxVM::new).collect(Collectors.toList());
+            return ResponseEntity.ok(newHoaxes);
+        }
+
+        return ResponseEntity.ok(hoaxService.getOldHoaxes(id, username, page).map(HoaxVM::new));
     }
 }
--- src/main/java/com/urbanlegend/hoax/HoaxRepository.java
+++ src/main/java/com/urbanlegend/hoax/HoaxRepository.java
@@ -4,8 +4,10 @@ import com.urbanlegend.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface HoaxRepository extends JpaRepository<Hoax,Long> {
+public interface HoaxRepository extends JpaRepository<Hoax,Long>, JpaSpecificationExecutor<Hoax> {
 
-    Page<Hoax> findByUser(User user, Pageable pageable);
+    Page<Hoax> findByUser(User user, Pageable page);
+
 }
