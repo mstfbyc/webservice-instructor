5687d4010106b11eea79198f8e69e5b7a998ef5f
Mustafa Boyaci <mustafaboyaci@hotmail.com>
2021-05-30 18:15:08 +0300

    Proje Bitmiştir...

---
+++ src/main/resources/static/precache-manifest.9f012fab9732ba8102417a331432d648.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5b062fa31cc3a7d36857166cf05823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f308481ad08cece2b7",
+    "url": "/static/css/main.1cbe92e7.chunk.css"
+  },
+  {
+    "revision": "4a8b20df60af978e475b",
+    "url": "/static/js/2.e042978d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e042978d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f308481ad08cece2b7",
+    "url": "/static/js/main.8b073dbf.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "981915133f2807a8fea44205a0ba4a9f",
+    "url": "/static/media/hoaxify.98191513.png"
+  },
+  {
+    "revision": "3b1b17bcf99b1f841d6bbdf6fd6d80fb",
+    "url": "/static/media/profile.3b1b17bc.png"
+  }
+]);
